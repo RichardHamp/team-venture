eae1f8e131c27870720aa3a41a30b5c24c5e7226
Nathaniel Anderson <nathanielmotion@gmail.com>
2019-11-16 11:01:00 -0700

    AJAX and javascript link in all tabs

---
+++ app.js
@@ -0,0 +1,15 @@
+//Dictionary API
+$.ajax({
+    url: "https://www.dictionaryapi.com/api/v3/references/collegiate/json/voluminous?key=ce96d9e4-de5d-4795-8723-7c3340d395de",
+    method: "GET"}).then(function(response) {
+        console.log(response);
+      });
+
+//Thesaurus API
+$.ajax({
+    url: "https://words.bighugelabs.com/api/2/9670eec22c87195e1d58c8571bc3859c/cat/json",
+    method: "GET"}).then(function(response) {
+        console.log(response);
+      });      
+
+
